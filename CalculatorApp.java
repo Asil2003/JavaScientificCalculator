353c617ade66fab2dc3e0e8556572a80745f40d0
Asil2003 <asilalabri03@gmail.com>
2025-07-20 13:57:44 +0400

    Step 2: Added Scanner input for a number

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class CalculatorApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Java Scientific Calculator!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+        System.out.println("You entered: " + number);
+        scanner.close();
     }
 }
