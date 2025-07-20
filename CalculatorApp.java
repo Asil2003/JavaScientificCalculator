0c9b9da2213fb333339fc6109bd648c0e685af61
Asil2003 <asilalabri03@gmail.com>
2025-07-20 13:59:15 +0400

    Step 4: Added Math.pow() operation

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -11,5 +11,13 @@ public class CalculatorApp {
         double result = Math.sqrt(number);
         System.out.println("Square root: " + result);
 
+        System.out.print("Enter base: ");
+        double base = scanner.nextDouble();
+        System.out.print("Enter exponent: ");
+        double exponent = scanner.nextDouble();
+        double powerResult = Math.pow(base, exponent);
+        System.out.println("Power result: " + powerResult);
+
+
     }
 }
